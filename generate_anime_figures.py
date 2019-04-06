9ba1949f12025df6c2fbca56f9fe628a92f6e1ac
David Leonardi <davidleonardi418@gmail.com>
2019-04-06 18:49:39 +0800

    Generate animated video

--- generate_anime_figures.py
+++ generate_anime_figures.py
@@ -9,12 +9,11 @@
 
 import os
 import pickle
-
-import PIL.Image
+import scipy
+import moviepy.editor
 import numpy as np
 
 import config
-import dnnlib
 import dnnlib.tflib as tflib
 
 # ----------------------------------------------------------------------------
@@ -26,122 +25,73 @@ synthesis_kwargs = dict(output_transform=dict(func=tflib.convert_images_to_uint8
 
 _Gs_cache = dict()
 
+grid_size = [5, 3]
+image_shrink = 1
+image_zoom = 1
+duration_sec = 30.0
+smoothing_sec = 1.0
+mp4_fps = 60
+mp4_codec = 'libx264'
+mp4_bitrate = '16M'
+random_seed = 404
+mp4_file = 'results/random_grid_%s.mp4' % random_seed
+minibatch_size = 8
+
 
 def load_Gs(url):
     if url not in _Gs_cache:
         with open(url, 'rb') as f:
-        # with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
             _G, _D, Gs = pickle.load(f)
         _Gs_cache[url] = Gs
     return _Gs_cache[url]
 
 
-# ----------------------------------------------------------------------------
-# Figures 2, 3, 10, 11, 12: Multi-resolution grid of uncurated result images.
+def gen_animated_faces(Gs):
+    num_frames = int(np.rint(duration_sec * mp4_fps))
+    random_state = np.random.RandomState(random_seed)
 
-def draw_uncurated_result_figure(png, Gs, cx, cy, cw, ch, rows, lods, seed):
-    print(png)
-    latents = np.random.RandomState(seed).randn(sum(rows * 2 ** lod for lod in lods), Gs.input_shape[1])
-    images = Gs.run(latents, None, **synthesis_kwargs)  # [seed, y, x, rgb]
+    # Generate latent vectors
+    shape = [num_frames, np.prod(grid_size)] + Gs.input_shape[1:]  # [frame, image, channel, component]
+    all_latents = random_state.randn(*shape).astype(np.float32)
+    all_latents = scipy.ndimage.gaussian_filter(all_latents, [smoothing_sec * mp4_fps] + [0] * len(Gs.input_shape),
+                                                mode='wrap')
+    all_latents /= np.sqrt(np.mean(np.square(all_latents)))
 
-    canvas = PIL.Image.new('RGB', (sum(cw // 2 ** lod for lod in lods), ch * rows), 'white')
-    image_iter = iter(list(images))
-    for col, lod in enumerate(lods):
-        for row in range(rows * 2 ** lod):
-            image = PIL.Image.fromarray(next(image_iter), 'RGB')
-            image = image.crop((cx, cy, cx + cw, cy + ch))
-            image = image.resize((cw // 2 ** lod, ch // 2 ** lod), PIL.Image.ANTIALIAS)
-            canvas.paste(image, (sum(cw // 2 ** lod for lod in lods[:col]), row * ch // 2 ** lod))
-    canvas.save(png)
+    # Frame generation func for moviepy.
+    def make_frame(t):
+        frame_idx = int(np.clip(np.round(t * mp4_fps), 0, num_frames - 1))
+        latents = all_latents[frame_idx]
+        fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+        images = Gs.run(latents, None, truncation_psi=0.7,
+                        randomize_noise=False, output_transform=fmt)
 
+        grid = create_image_grid(images, grid_size)
+        if image_zoom > 1:
+            grid = scipy.ndimage.zoom(grid, [image_zoom, image_zoom, 1], order=0)
+        if grid.shape[2] == 1:
+            grid = grid.repeat(3, 2)  # grayscale => RGB
+        return grid
 
-# ----------------------------------------------------------------------------
-# Figure 3: Style mixing.
-
-def draw_style_mixing_figure(png, Gs, w, h, src_seeds, dst_seeds, style_ranges):
-    print(png)
-    src_latents = np.stack(np.random.RandomState(seed).randn(Gs.input_shape[1]) for seed in src_seeds)
-    dst_latents = np.stack(np.random.RandomState(seed).randn(Gs.input_shape[1]) for seed in dst_seeds)
-    src_dlatents = Gs.components.mapping.run(src_latents, None)  # [seed, layer, component]
-    dst_dlatents = Gs.components.mapping.run(dst_latents, None)  # [seed, layer, component]
-    src_images = Gs.components.synthesis.run(src_dlatents, randomize_noise=False, **synthesis_kwargs)
-    dst_images = Gs.components.synthesis.run(dst_dlatents, randomize_noise=False, **synthesis_kwargs)
-
-    canvas = PIL.Image.new('RGB', (w * (len(src_seeds) + 1), h * (len(dst_seeds) + 1)), 'white')
-    for col, src_image in enumerate(list(src_images)):
-        canvas.paste(PIL.Image.fromarray(src_image, 'RGB'), ((col + 1) * w, 0))
-    for row, dst_image in enumerate(list(dst_images)):
-        canvas.paste(PIL.Image.fromarray(dst_image, 'RGB'), (0, (row + 1) * h))
-        row_dlatents = np.stack([dst_dlatents[row]] * len(src_seeds))
-        row_dlatents[:, style_ranges[row]] = src_dlatents[:, style_ranges[row]]
-        row_images = Gs.components.synthesis.run(row_dlatents, randomize_noise=False, **synthesis_kwargs)
-        for col, image in enumerate(list(row_images)):
-            canvas.paste(PIL.Image.fromarray(image, 'RGB'), ((col + 1) * w, (row + 1) * h))
-    canvas.save(png)
+    video_clip = moviepy.editor.VideoClip(make_frame, duration=duration_sec)
+    video_clip.write_videofile(mp4_file, fps=mp4_fps, codec=mp4_codec, bitrate=mp4_bitrate)
 
 
-# ----------------------------------------------------------------------------
-# Figure 4: Noise detail.
+def create_image_grid(images, grid_size=None):
+    assert images.ndim == 3 or images.ndim == 4
+    num, img_h, img_w, channels = images.shape
 
-def draw_noise_detail_figure(png, Gs, w, h, num_samples, seeds):
-    print(png)
-    canvas = PIL.Image.new('RGB', (w * 3, h * len(seeds)), 'white')
-    for row, seed in enumerate(seeds):
-        latents = np.stack([np.random.RandomState(seed).randn(Gs.input_shape[1])] * num_samples)
-        images = Gs.run(latents, None, truncation_psi=1, **synthesis_kwargs)
-        canvas.paste(PIL.Image.fromarray(images[0], 'RGB'), (0, row * h))
-        for i in range(4):
-            crop = PIL.Image.fromarray(images[i + 1], 'RGB')
-            crop = crop.crop((650, 180, 906, 436))
-            crop = crop.resize((w // 2, h // 2), PIL.Image.NEAREST)
-            canvas.paste(crop, (w + (i % 2) * w // 2, row * h + (i // 2) * h // 2))
-        diff = np.std(np.mean(images, axis=3), axis=0) * 4
-        diff = np.clip(diff + 0.5, 0, 255).astype(np.uint8)
-        canvas.paste(PIL.Image.fromarray(diff, 'L'), (w * 2, row * h))
-    canvas.save(png)
+    if grid_size is not None:
+        grid_w, grid_h = tuple(grid_size)
+    else:
+        grid_w = max(int(np.ceil(np.sqrt(num))), 1)
+        grid_h = max((num - 1) // grid_w + 1, 1)
 
-
-# ----------------------------------------------------------------------------
-# Figure 5: Noise components.
-
-def draw_noise_components_figure(png, Gs, w, h, seeds, noise_ranges, flips):
-    print(png)
-    Gsc = Gs.clone()
-    noise_vars = [var for name, var in Gsc.components.synthesis.vars.items() if name.startswith('noise')]
-    noise_pairs = list(zip(noise_vars, tflib.run(noise_vars)))  # [(var, val), ...]
-    latents = np.stack(np.random.RandomState(seed).randn(Gs.input_shape[1]) for seed in seeds)
-    all_images = []
-    for noise_range in noise_ranges:
-        tflib.set_vars({var: val * (1 if i in noise_range else 0) for i, (var, val) in enumerate(noise_pairs)})
-        range_images = Gsc.run(latents, None, truncation_psi=1, randomize_noise=False, **synthesis_kwargs)
-        range_images[flips, :, :] = range_images[flips, :, ::-1]
-        all_images.append(list(range_images))
-
-    canvas = PIL.Image.new('RGB', (w * 2, h * 2), 'white')
-    for col, col_images in enumerate(zip(*all_images)):
-        canvas.paste(PIL.Image.fromarray(col_images[0], 'RGB').crop((0, 0, w // 2, h)), (col * w, 0))
-        canvas.paste(PIL.Image.fromarray(col_images[1], 'RGB').crop((w // 2, 0, w, h)), (col * w + w // 2, 0))
-        canvas.paste(PIL.Image.fromarray(col_images[2], 'RGB').crop((0, 0, w // 2, h)), (col * w, h))
-        canvas.paste(PIL.Image.fromarray(col_images[3], 'RGB').crop((w // 2, 0, w, h)), (col * w + w // 2, h))
-    canvas.save(png)
-
-
-# ----------------------------------------------------------------------------
-# Figure 8: Truncation trick.
-
-def draw_truncation_trick_figure(png, Gs, w, h, seeds, psis):
-    print(png)
-    latents = np.stack(np.random.RandomState(seed).randn(Gs.input_shape[1]) for seed in seeds)
-    dlatents = Gs.components.mapping.run(latents, None)  # [seed, layer, component]
-    dlatent_avg = Gs.get_var('dlatent_avg')  # [component]
-
-    canvas = PIL.Image.new('RGB', (w * len(psis), h * len(seeds)), 'white')
-    for row, dlatent in enumerate(list(dlatents)):
-        row_dlatents = (dlatent[np.newaxis] - dlatent_avg) * np.reshape(psis, [-1, 1, 1]) + dlatent_avg
-        row_images = Gs.components.synthesis.run(row_dlatents, randomize_noise=False, **synthesis_kwargs)
-        for col, image in enumerate(list(row_images)):
-            canvas.paste(PIL.Image.fromarray(image, 'RGB'), (col * w, row * h))
-    canvas.save(png)
+    grid = np.zeros([grid_h * img_h, grid_w * img_w, channels], dtype=images.dtype)
+    for idx in range(num):
+        x = (idx % grid_w) * img_w
+        y = (idx // grid_w) * img_h
+        grid[y: y + img_h, x: x + img_w] = images[idx]
+    return grid
 
 
 # ----------------------------------------------------------------------------
@@ -150,9 +100,8 @@ def draw_truncation_trick_figure(png, Gs, w, h, seeds, psis):
 def main():
     tflib.init_tf()
     os.makedirs(config.result_dir, exist_ok=True)
-    draw_uncurated_result_figure(os.path.join(config.result_dir, 'figure02-uncurated-anime-faces-256.png'),
-                                 load_Gs(anime_faces_256_url), cx=0, cy=0, cw=256, ch=256, rows=3,
-                                 lods=[0, 1, 2, 2, 3, 3], seed=5)
+    gen_animated_faces(load_Gs(anime_faces_256_url))
+    # draw_uncurated_result_figure(os.path.join(config.result_dir, 'figure02-uncurated-anime-faces-256.png'), load_Gs(anime_faces_256_url), cx=0, cy=0, cw=256, ch=256, rows=3, lods=[0, 1, 2, 2, 3, 3], seed=5)
     # draw_style_mixing_figure(os.path.join(config.result_dir, 'figure03-style-mixing.png'), load_Gs(url_ffhq), w=1024, h=1024, src_seeds=[639,701,687,615,2268], dst_seeds=[888,829,1898,1733,1614,845], style_ranges=[range(0,4)]*3+[range(4,8)]*2+[range(8,18)])
     # draw_noise_detail_figure(os.path.join(config.result_dir, 'figure04-noise-detail.png'), load_Gs(url_ffhq), w=1024, h=1024, num_samples=100, seeds=[1157,1012])
     # draw_noise_components_figure(os.path.join(config.result_dir, 'figure05-noise-components.png'), load_Gs(url_ffhq), w=1024, h=1024, seeds=[1967,1555], noise_ranges=[range(0, 18), range(0, 0), range(8, 18), range(0, 8)], flips=[1])
